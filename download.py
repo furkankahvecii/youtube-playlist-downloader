ba88019fb657b53d1e21d149fe49b514790ac254
furkankahvecii <furkankahveci@hotmail.com.tr>
2020-08-11 15:03:42 +0300

    The download start number added and functions have adapted to the Single responsibility rule.

--- download.py
+++ download.py
@@ -24,21 +24,23 @@ class DownloadLocators(object):
     ERROR_MESSAGE = (By.CSS_SELECTOR, '#error')
 
 class YoutubePlaylistDownload():
-    def __init__(self, driver):
+    def __init__(self, driver, url, start = 0):
         self.driver = driver
         self.links = {}
+        self.url = url
+        self.start = start
         self.get_videos()
 
     def control_url(self):
         try:
-            self.driver.get(url)
+            self.driver.get(self.url)
             return self.driver.current_url != "https://www.youtube.com/oops" if True else False
         except:
             return False
 
     def get_videos(self):
         self.driver.implicitly_wait(3)
-        assert self.control_url(self.driver), "Invalid url!"
+        assert self.control_url(), "Invalid url!"
         assert self.driver.find_element(*DownloadLocators().PRIVATE_ERROR).get_attribute('innerHTML') != "This playlist is private.", "Playlist is private."
 
         playlist_video_count = re.search(r'\d+', self.driver.find_element(*YoutubeLocators().COUNT_VIDEO).get_attribute('innerHTML')).group(0)
@@ -47,18 +49,26 @@ class YoutubePlaylistDownload():
             self.driver.execute_script("var scrollingElement = (document.scrollingElement || document.body);scrollingElement.scrollTop = scrollingElement.scrollHeight;")  
             time.sleep(3) 
 
-        all_videos = self.driver.find_elements(*YoutubeLocators().ALL_LIST)
+        self.check_private_delete()
+        self.download_all()
+
+    def check_private_delete(self):
+        all_videos = self.calculate_start_video()
         for video in all_videos:
             video_title = video.find_element_by_css_selector('#video-title').get_attribute('innerHTML').strip() # strip == trim
             if video_title != '[Deleted video]' and video_title != '[Private video]' :
                 self.links[video.find_element(*YoutubeLocators().EACH_VIDEO).get_attribute('href')] = None
-        
-        self.download_all()
 
-    
+    def calculate_start_video(self):
+        all_videos = self.driver.find_elements(*YoutubeLocators().ALL_LIST)
+        if(len(all_videos) > self.start and self.start >= 0):
+            del all_videos[0:self.start]
+        return all_videos
+
     def download_all(self):
         assert len(self.links) != 0 , "Not found video that downloadable"
         print(str(len(self.links)) + " videos will be converted to mp3 format.\nDownload begins.")
+
         for link in self.links:
             self.links[link] = self.download(link) if True else None
 
@@ -91,7 +101,6 @@ class YoutubePlaylistDownload():
         time.sleep(2)
         return True
 
-    
     def check_exists_element(self, element):
         try:
             self.driver.find_element(*element)
@@ -105,5 +114,7 @@ class YoutubePlaylistDownload():
 
 
 url = input("Enter your playlist url: ")
-brow = webdriver.Chrome(executable_path="your/chromedriver/path")
-instance = YoutubePlaylistDownload(brow,url)
+brow = webdriver.Chrome(executable_path="C:/Users/Furkan Kahveci/Desktop/deneme/alzare/chromedriver.exe")
+start = 0
+
+instance = YoutubePlaylistDownload(brow, url, start)
